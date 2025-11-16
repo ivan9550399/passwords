1c004b0797b53fa2a383d490dc6cb062827dff15
Ivan Stanchevskiy <mailyp@ro.ru>
2025-11-16 12:28:55 +0300

    комит

--- pw_manager.py
+++ pw_manager.py
@@ -16,6 +16,7 @@ from cryptography.hazmat.primitives import hashes
 from termcolor import colored
 
 
+
 # Конфигурация приложения: пути, логирование и символы для паролей
 class Config:
     APP_FOLDER = Path(os.getenv("APPDATA", os.path.expanduser("~"))) / "pw_manager"
